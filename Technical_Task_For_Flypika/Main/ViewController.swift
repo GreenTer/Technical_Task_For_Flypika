b16b695a3e044425c4562b973b1cb0b8aeb83602
GreenTer <thenewlife@tut.by>
2021-06-25 16:51:48 +0300

    add TabBar + ProfileInfo

--- Technical_Task_For_Flypika/Main/ViewController.swift
+++ Technical_Task_For_Flypika/Main/ViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // при загрузке приложения, делаем переход (позже добавлю пользователя)
+    // появится Navigation Bar + Tab Bar
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
+        // переход со Storyboard на View Controller 
+        let mainStoryboard = UIStoryboard.init(name: "TabBar", bundle: nil)
+        // делаем связь View Controller со Storyboard
+        let viewController = mainStoryboard.instantiateViewController(identifier: "TabBar")
+        // переход на View Controller
+        self.navigationController?.pushViewController(viewController, animated: false)
     }
 }
---
+++ Technical_Task_For_Flypika/TabBar/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  Technical_Task_For_Flypika
+//
+//  Created by Baravitski Aliaksandr on 25.06.21.
+//
+
+import UIKit
+
+// контроллер, отвечающий за внутренности TabBar
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() { 
+        super.viewDidLoad()
+
+        // скрыть кнопку Back
+//        self.navigationItem.hidesBackButton = true
+     
+    }
+}
