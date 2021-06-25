7314decc328f8330f686b851cca855c6f9db271c
GreenTer <thenewlife@tut.by>
2021-06-25 13:48:04 +0300

    initial

---
+++ Technical_Task_For_Flypika/Main/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Technical_Task_For_Flypika
+//
+//  Created by Baravitski Aliaksandr on 25.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+}
