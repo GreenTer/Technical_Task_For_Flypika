b93c31672589d7bac2b4da1b6595cb6d83ff6a5c
GreenTer <thenewlife@tut.by>
2021-06-27 03:06:22 +0300

    added Log-In

---
+++ Technical_Task_For_Flypika/Application/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  Technical_Task_For_Flypika
+//
+//  Created by Baravitski Aliaksandr on 26.06.21.
+//
+
+import Foundation
---
+++ Technical_Task_For_Flypika/PicaChu/PicaChu.swift
@@ -0,0 +1,29 @@
+//
+//  PicaChu.swift
+//  Technical_Task_For_Flypika
+//
+//  Created by Baravitski Aliaksandr on 27.06.21.
+//
+
+import UIKit
+
+class PicaChu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
