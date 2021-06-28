eb15d86c57ab55a8f22de25b90ae7b1d8c6f1e09
GreenTer <thenewlife@tut.by>
2021-06-28 22:34:45 +0300

    update design v2

--- Technical_Task_For_Flypika/NewsFeed/NewsFeedCell/NewsFeedCell.swift
+++ Technical_Task_For_Flypika/NewsFeed/NewsFeedCell/NewsFeedCell.swift
@@ -12,14 +12,14 @@ class NewsfeedCell: UITableViewCell {
     
     static let reuseId = "NewsFeedCell"
     
-//    @IBOutlet weak var iconImageView: UIImageView!
-//    @IBOutlet weak var namelabel: UILabel!
-//    @IBOutlet weak var dateLabel: UILabel!
-//    @IBOutlet weak var postlabel: UILabel!
-//    @IBOutlet weak var likesLabel: UILabel!
-//    @IBOutlet weak var commentsLabel: UILabel!
-//    @IBOutlet weak var shareslabel: UILabel!
-//    @IBOutlet weak var viewsLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var postLabel: UILabel!
+    @IBOutlet weak var likesLabel: UIView!
+    @IBOutlet weak var commentsLabel: UIView!
+    @IBOutlet weak var sharesLabel: UIView!
+    @IBOutlet weak var viewsLabel: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
